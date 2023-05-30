a514965b4045e09402d49b49f2b7b6590b9111b5
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-05-30 10:06:07 -0700

    printf

---
+++ wowget_size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_size - Calculates sizes to cast to the argument
+ * @format: Format the string to print the arguments
+ * @i: List of the arguments to print.
+ *
+ * Return: The Precision.
+ */
+	int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+	size = S_LONG;
+	else if (format[curr_i] == 'h')
+	size = S_SHORT;
+
+	if (size == 0)
+	*i = curr_i - 1;
+	else
+	*i = curr_i;
+
+	{return (size);
+
+}
